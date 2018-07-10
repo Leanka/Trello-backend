698ac9c65b63131040923b07b4054784004d1d65
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-10 11:16:09 +0200

    Fill up delete() with db method call

--- controller/users.js
+++ controller/users.js
@@ -7,4 +7,6 @@ exports.index = (req, res) => {
 exports.show = (req, res) => {} 
 exports.crete = (req, res) => {} 
 exports.update = (req, res) => {} 
-exports.delete = (req, res) => {} 
+exports.delete = (req, res) => {
+    DatabaseConnection.removeUser(req, res);
+} 
