5bdba1895758df75b16bde7e2ff2c3d9242993dc
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-10 10:40:10 +0200

    Add database connection; Fill up index function with db method call

--- controller/users.js
+++ controller/users.js
@@ -1,5 +1,9 @@
+var DatabaseConnection = require(".././db.js");
+DatabaseConnection.connectDb();
 
-exports.index = (req, res) => {} 
+exports.index = (req, res) => {
+    DatabaseConnection.getAllUsers(req, res);
+} 
 exports.show = (req, res) => {} 
 exports.crete = (req, res) => {} 
 exports.update = (req, res) => {} 
