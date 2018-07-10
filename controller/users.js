50bba8dfd8dc2a68655aadd5ba11ea46e31bc5ba
Kamil Boksa <kamil.boksa@gmail.com>
2018-07-10 09:13:52 +0000

    Handle user/:id route

--- controller/users.js
+++ controller/users.js
@@ -4,7 +4,9 @@ DatabaseConnection.connectDb();
 exports.index = (req, res) => {
     DatabaseConnection.getAllUsers(req, res);
 } 
-exports.show = (req, res) => {} 
+exports.show = (req, res) => {
+    DatabaseConnection.getUserById(req, res);
+} 
 exports.crete = (req, res) => {} 
 exports.update = (req, res) => {} 
 exports.delete = (req, res) => {} 
