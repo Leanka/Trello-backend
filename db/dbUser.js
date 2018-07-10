fcb0da47403427f50b1c499e3023bbc1a35c6e4c
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-10 14:38:03 +0200

    Create separate file from methods removed from db.js

---
+++ db/dbUser.js
@@ -0,0 +1,83 @@
+let mongoose = require("mongoose");
+let User = require(".././models/user.js");
+var UserDatabase = {}
+
+UserDatabase.addNewUserToDb = function(req, res) {
+    let user = new User({ username: req.body.username,
+                          password: req.body.password,
+                          projects_ids: req.body.projects_ids
+                        });
+    user.save(function (err) {
+    if (err) {
+        return console.error(err);
+    } else {
+        console.log("New user added successfully");  
+    }
+  });
+}
+
+UserDatabase.showAllUsers = function() {
+    User.find(function(err, allUsers) {
+        if(err) {
+            return console.error(err);
+        } else {
+            console.log(allUsers);
+        }
+    });
+}
+
+UserDatabase.removeAllUsers = function() {
+    User.remove({}, function(err) { 
+    if(err) {
+        console.log("cannot clear db");
+    }
+  console.log('collection removed') 
+});
+}
+
+UserDatabase.getAllUsers = function(req, res) {
+-   User.find(function(err, allUsers) {
+        if(err) {
+            return console.error(err);
+        } else {
+            res.json(allUsers)
+            res.end()
+        }
+    });
+}
+
+UserDatabase.removeUser = function(req, res){
+    let userId = req.params.id;
+    User.findByIdAndRemove(userId, (err) => {
+        if(err){
+            console.log("Cannot find given user");
+        }
+        res.json(userId)
+        res.end();
+    })
+}
+
+UserDatabase.updateUser = function(req, res){
+    let userId = req.params.id;
+    let dataToUpdate = req.body;
+
+    User.findByIdAndUpdate(userId, dataToUpdate, (err) => {
+        if(err){
+            console.log("Cannot find given user");
+        }
+        this.getUserById(req, res);
+    })
+}
+
+UserDatabase.getUserById = function(req, res) {
+    let userId = req.params.id;
+    User.findById(userId, function (err, foundUser) {
+        if(err) {
+            console.log("Cannot find given user");
+        } else {
+            res.json(foundUser);
+        }
+    });
+}
+
+module.exports = UserDatabase;
