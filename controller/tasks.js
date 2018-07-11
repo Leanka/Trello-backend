a283c0a8bcda800d70eff73435f714f56cfe189e
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-11 14:56:16 +0200

    Implement show, update and remove methods

--- controller/tasks.js
+++ controller/tasks.js
@@ -3,6 +3,12 @@ var TaskDatabase = require(".././db/dbTasks.js");
 exports.index = (req, res) => {};
 exports.create = (req, res) => {};
 
-exports.show = (req, res) => {};
-exports.update = (req, res) => {};
-exports.remove = (req, res) => {};
+exports.show = (req, res) => {
+    TaskDatabase.getTaskById(req, res);
+};
+exports.update = (req, res) => {
+    TaskDatabase.updaterTask(req, res);
+};
+exports.remove = (req, res) => {
+    TaskDatabase.removeTask(req, res);
+};
