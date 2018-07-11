9b691223eb82060bb07305dc36a0a764be187dbe
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-11 20:00:02 +0200

    Rewrite all methods to receive Promiss object; Pass them only required data istead of request and response

--- controller/users.js
+++ controller/users.js
@@ -1,18 +1,36 @@
 var UserDatabase = require(".././db/dbUser.js");
+let User = require(".././models/user.js");
 
-exports.index = (req, res) => {
-    UserDatabase.getAllUsers(req, res);
+exports.index = (res) => {
+    UserDatabase.getAllUsers().then((result)=>{
+        res.json(result)
+        res.end()
+    })
 } 
 exports.show = (req, res) => {
-    UserDatabase.getUserById(req, res);
+    UserDatabase.getUserById(req.params.id).then((result) => {
+        res.json(result)
+        res.end()
+    });
 } 
 exports.create = (req, res) => {
-    UserDatabase.addNewUserToDb(req, res);    
+    let user = new User({ 
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    UserDatabase.addNewUserToDb(user).then(() => {
+        res.end()
+    })  
 }
 
 exports.update = (req, res) => {
-    UserDatabase.updateUser(req, res);
+    UserDatabase.updateUser(req.params.id, req.body).then(() => {
+        res.end()
+    })
 } 
 exports.delete = (req, res) => {
-    UserDatabase.removeUser(req, res);
+    UserDatabase.removeUser(req.params.id).then(() => {
+        res.end()
+    })
 } 
