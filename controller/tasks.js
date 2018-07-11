e763201f32646cf9b1d8ed1b6d31630e96629f60
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-11 14:51:40 +0200

    Create tasks controller frame

---
+++ controller/tasks.js
@@ -0,0 +1,8 @@
+var TaskDatabase = require(".././db/dbTasks.js");
+
+exports.index = (req, res) => {};
+exports.create = (req, res) => {};
+
+exports.show = (req, res) => {};
+exports.update = (req, res) => {};
+exports.remove = (req, res) => {};
