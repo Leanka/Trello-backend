ca26de4b707e3ed089e5692f15a1ff1329d94bad
Kamil Boksa <kamil.boksa@gmail.com>
2018-07-11 08:53:35 +0000

    Implement getting all projects

--- db/dbProject.js
+++ db/dbProject.js
@@ -22,14 +22,21 @@ ProjectDatabase.addNewProject = function(req, res) {
 }
 
 ProjectDatabase.getUserProjects = function(req, res) {
-    // let author = {
-    //     id: req.params.id
-    // }
     Project.find({"author.id":req.params.id}, function(err, userProjects) {
         if(err) {
             console.log(err);
         } else {
           res.json(userProjects);  
+        }
+    })
+}
+
+ProjectDatabase.getAllProjects = function(req, res) {
+    Project.find(function(err, allProjects) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.json(allProjects);
         }
     })
 }
