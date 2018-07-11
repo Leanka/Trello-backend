a2e8c84b9c3b72f0f4448d0c09a9275e1f745aa9
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-11 15:02:09 +0200

    Implement updaterTask()

--- db/dbTasks.js
+++ db/dbTasks.js
@@ -6,7 +6,17 @@ TaskDatabase.getListTasks = (req, res)=>{};
 TaskDatabase.getTaskById = (req, res)=>{};
 TaskDatabase.getAllTasks = (req, res)=>{};
 
-TaskDatabase.updaterTask = (req, res)=>{};
+TaskDatabase.updaterTask = (req, res)=>{
+    let taskId = req.params.id;
+    let dataToUpdate = req.body;
+
+    Task.findByIdAndUpdate(taskId, dataToUpdate, (err) => {
+        if(err){
+            console.log("Cannot update given task");
+        }
+        return this.getTaskById(req, res);
+    })
+};
 TaskDatabase.removeTask = (req, res)=>{};
 TaskDatabase.removeAllTasksFromList = (req, res)=>{};
 
