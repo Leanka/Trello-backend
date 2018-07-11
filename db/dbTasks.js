39142602b147a40d58ff3c16d37c0b72b390d034
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-11 21:03:49 +0200

    Wrap all methods in Promiss object; Update parameters lists to get only required data instead of request and response

--- db/dbTasks.js
+++ db/dbTasks.js
@@ -1,80 +1,75 @@
 let Task = require(".././models/task.js");
 var TaskDatabase = {};
 
-TaskDatabase.addNewTask = (req, res)=>{
-    let parentList = {
-                       id : req.params.id
-                     }
-
-    let newTask = { 
-                    title : req.body.title,
-                    parentList : parentList
-                  }
-    
-    Task.create(newTask, function(err, newlyCreated) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("New task created successfully");
-            res.end();
-        }
+TaskDatabase.addNewTask = (newTask)=>{
+    return new Promise((resolve, reject) => {
+        Task.create(newTask, function(err, newlyCreated) {
+            if (err) {
+                return reject(err)
+            } else {
+                return resolve(newlyCreated);
+            }
+        })
     })
 };
 
-TaskDatabase.getListTasks = (req, res)=>{
-        Task.find({"parentList.id":req.params.id}, function(err, listTasks) {
-        if(err) {
-            console.log(err);
-        } else {
-          res.json(listTasks);  
-        }
+TaskDatabase.getListTasks = (listId)=>{
+    return new Promise((resolve, reject) => {
+        Task.find({"parentList.id":listId}, function(err, listTasks) {
+            if (err) {
+                return reject(err)
+            } else {
+                return resolve(listTasks);
+            }
+        })
     })
 };
 
-TaskDatabase.getTaskById = (req, res)=>{
-    let taskId = req.params.id;
-
-    Task.findById(taskId, (err, task) => {
-        if(err){
-            console.log("Cannot find given task");
-        }
-        res.json(task);
-        res.end()
+TaskDatabase.getTaskById = (taskId)=>{
+    return new Promise((resolve, reject) => {
+        Task.findById(taskId, (err, task) => {
+            if (err) {
+                return reject(err)
+            } else {
+                return resolve(task);
+            }
+        })
     })
 };
 
-TaskDatabase.getAllTasks = (req, res)=>{
-    Task.find({}, (err, tasks) =>{
-        if(err){
-            console.log("Cannot get task at all");
-        }
-        res.json(tasks);
-        res.end()
+TaskDatabase.getAllTasks = ()=>{
+    return new Promise((resolve, reject) => {
+        Task.find({}, (err, tasks) =>{
+            if (err) {
+                return reject(err)
+            } else {
+                return resolve(tasks);
+            }
+        })
     })
 };
 
-TaskDatabase.updateTask = (req, res)=>{
-    let taskId = req.params.id;
-    let dataToUpdate = req.body;
-
-    Task.findByIdAndUpdate(taskId, dataToUpdate, (err) => {
-        if(err){
-            console.log("Cannot update given task");
-        }
-        res.json("Updated task with id " + taskId);
-        res.end();
-        // this.getTaskById(req, res);
+TaskDatabase.updateTask = (taskId, dataToUpdate)=>{
+    return new Promise((resolve, reject) => {
+        Task.findByIdAndUpdate(taskId, dataToUpdate, (err) => {
+            if (err) {
+                return reject(err)
+            } else {
+                return resolve();
+            }
+        })
     })
 };
-TaskDatabase.removeTask = (req, res)=>{
-    let taskId = req.params.id;
 
-    Task.findByIdAndRemove(taskId, (err) => {
-        if(err){
-            console.log("Cannot remove given task");
-        }
-        res.json("Removed task with id " + taskId);
-        res.end();
+TaskDatabase.removeTask = (taskId)=>{
+    return new Promise((resolve, reject) => {
+        Task.findByIdAndRemove(taskId, (err) => {
+            if (err) {
+                return reject(err)
+            } else {
+                return resolve();
+            }
+        })
     })
 };
 
