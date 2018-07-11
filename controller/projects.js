cd30d43bccb762aa3e5cbf193d0b3916ec67eaa8
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-11 20:30:18 +0200

    Rewrite all methods to receive Promiss object; Pass them only required data istead of request and response

--- controller/projects.js
+++ controller/projects.js
@@ -1,25 +1,47 @@
 var ProjectDatabase = require(".././db/dbProject.js");
 
 exports.index = (req, res) => {
-    ProjectDatabase.getUserProjects(req, res);
+    ProjectDatabase.getUserProjects(req.params.id).then((result) => {
+        res.json(result)
+        res.end()
+    })
 }
 
 exports.show = (req, res) => {
-    ProjectDatabase.getProjectById(req,res);
+    ProjectDatabase.getProjectById(req.params.id).then((result) => {
+        res.json(result)
+        res.end()
+    })
 }
 
 exports.create = (req, res) => {
-    ProjectDatabase.addNewProject(req, res);
+    let author = { id : req.params.id }
+
+    let newProject = { title : req.body.title,
+                       description : req.body.description,
+                       author : author
+    }
+
+    ProjectDatabase.addNewProject(newProject).then(() => {
+        res.end()
+    })
 }
 
 exports.update = (req, res) => {
-    ProjectDatabase.updateProject(req, res);
+    ProjectDatabase.updateProject(req.params.id, req.body).then(() => {
+        res.end()
+    })
 }
 
 exports.delete = (req, res) => {
-    ProjectDatabase.removeProject(req, res);
+    ProjectDatabase.removeProject(req.params.id).then(() => {
+        res.end()
+    })
 }
 
-exports.showAll = (req,res) => {
-    ProjectDatabase.getAllProjects(req, res);
+exports.showAll = (res) => {
+    ProjectDatabase.getAllProjects().then((result) => {
+        res.json(result)
+        res.end()
+    })
 }
