2289838adddc02a506f3c169fe2a9827e0391448
Kamil Boksa <kamil.boksa@gmail.com>
2018-07-12 09:31:16 +0000

    Add mongolab db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 let mongoose = require("mongoose");
 
 exports.connectDb = function() {
-    mongoose.connect("mongodb://localhost/trelolo_app");
+    //mongoose.connect("mongodb://localhost/trelolo_app");
+    mongoose.connect("mongodb://admin:admin123@ds235181.mlab.com:35181/trelolo");
     let db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
